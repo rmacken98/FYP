b7e52afeca28abd99c262021df4438764f5b9d45
Ronan Macken <ronan.macken@mydit.ie>
2020-03-26 22:17:34 +0000

    Add spot form

--- SpotForm.js
+++ SpotForm.js
@@ -10,36 +10,44 @@ import {withFormik} from 'formik';
 import * as yup from 'yup';
 import { addSpot, updateSpot, uploadSpot } from './SkateSpotsApi'
 import MyImagePicker from './components/MyImagePicker';
+import Firebase from './config/Firebase';
 
 
 const SpotForm = ( props)=> {
 
     setSpotImage = (image) => {
         props.setFieldValue('imageUri', image.uri);
-    }
-
-
+      }
+    // const spot = Spot context
+    // const k = spot.newMarker.longitude
+    //const x = spot.marker.latitude
+ const k = props.setLongitude
+ const x = props.setLatitude
+ const user = Firebase.auth().currentUser.email
+ 
 return (
     <View>
-         <MyImagePicker image={props.spot.image} onImagePicked={setSpotImage} />
+         <MyImagePicker image={props.spot.image} onImagePicked={setSpotImage}  />
     <TextInput
     
     placeholder='Spot name'
    
-    onChangeText = {text => { props.setFieldValue('name', text) }}
+    onChangeText = {text => { props.setFieldValue('name', text);  props.setFieldValue('longitude', k); props.setFieldValue('latitude', x), props.setFieldValue('createdBy',user) }}
     />
 
     <Text>{props.errors.name}</Text>
       <TextInput
     
     placeholder='Longitude'
-    value = {props.setLongitude}
-   // onChangeText = {text => {  props.setLongitude(text) }}
+     value =  {`${props.setLongitude}`}
+    // onChangeText = {text => { props.setFieldValue('longitude', k) }}
     />
     <Text>{props.errors.longitude}</Text>
     <TextInput
+   
     placeholder='Latitiude'
-    value = {props.setLatitude}
+     value = {`${props.setLatitude}`}
+    // onChangeText = {text => { props.setFieldValue('latitude', x) }}
     />
       <Text>{props.errors.latitude}</Text>
      
@@ -58,6 +66,7 @@ onPress={() => props.handleSubmit()}
 
 export default withFormik({
     mapPropsToValues: ()=>({name:'',
+    createdBy:'',
      longitude:0,
      latitude:0,
      imageUri: null
@@ -66,15 +75,19 @@ export default withFormik({
     enableReinitialize: true,
     validationSchema : (props) => yup.object().shape({
         name : yup.string().required(),
-        longitude: yup.number().required(),
-        latitude: yup.number().required()
+         longitude: yup.number().required(),
+         latitude: yup.number().required()
     }),
     handleSubmit: (values, {props}) => {
         if(props.spot.id){
         values.id = props.spot.id;
+       // values.latitude= 88
+        // values.latitude= props.setLatitude;
+        // values.longitude= props.setLongitude;
         values.createdAt = props.spot.createdAt;
         values.image= props.spot.image;
         uploadSpot(values, props.onSpotUpdated, {updating: true})
+        // this.props.navigation.navigate("SkateSpots")
 
         
     }
