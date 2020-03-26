643078bdc72a79c1d6e23d1a5e88637e3ad626f1
Ronan Macken <ronan.macken@mydit.ie>
2020-03-26 22:20:17 +0000

    edit to navigation screens

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import Login from './Login'
 import SignUp from './SignUp'
 import SkateSpots from './SkateSpots'
 import SpotFormScreen from './SpotFormScreen';
+import SpotFormScreen2 from './SpotFormScreen2';
+
 
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
@@ -13,10 +15,14 @@ import {
 } from "react-native";
 
 const AppStack = createStackNavigator({
-  SkateSpots: SkateSpots,
+  
   Login: Login,
   SignUp: SignUp,
-  SpotFormScreen: SpotFormScreen
+  SkateSpots: SkateSpots,
+  SpotFormScreen: SpotFormScreen,
+  SpotFormScreen2: SpotFormScreen2,
+
+
   
 });
 
