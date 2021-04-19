7db8fb89d7b29906b8a0fb81768d3fd14c23f76c
Ronan Macken <ronan.macken@mydit.ie>
2021-04-19 22:05:14 +0100

    Trick Data screen added

--- Sensors.js
+++ Sensors.js
@@ -9,9 +9,10 @@ import {
   View,
   Alert
   ,Picker,
-  ScrollView
+  ScrollView, 
+  Dimensions
 } from 'react-native';
-import {send, getMessages,getMyMessages} from './SkateSpotsApi';
+import { getTricks} from './SkateSpotsApi';
 import Firebase from "./config/Firebase";
 import {
     Accelerometer,
@@ -19,8 +20,9 @@ import {
     Gyroscope
   } from 'expo-sensors';
 import {Col, Grid, Row } from 'react-native-easy-grid';
+import {PieChart} from 'react-native-chart-kit';
 class Sensors extends Component {
-    
+  
   static navigationOptions = ({ navigation }) => {
 
    
@@ -31,7 +33,7 @@ class Sensors extends Component {
   };
 
   state = {
-
+    screenWidth : Dimensions.get("window").width,
       handRaised: false,
       DeviceData:[],    
       accelerometerData:{x: 0, y: 0, z: 0   },
@@ -46,20 +48,20 @@ class Sensors extends Component {
       tutorials:["https://www.youtube.com/watch?v=arDVW-aWWys","https://www.youtube.com/watch?v=s-Tw11EZCv4",
       "https://www.youtube.com/watch?v=EZhbRmYHjlI" ],
  
-      chartConfig = {
+      chartConfig : {
         backgroundGradientFrom: "#1E2923",
         backgroundGradientFromOpacity: 0,
         backgroundGradientTo: "#08130D",
         backgroundGradientToOpacity: 0.5,
         color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
         strokeWidth: 2, // optional, default 3
-        barPercentage: 0.5,
-        useShadowColorFromDataset: false // optional
+        decimalPlaces: 3,   
+             useShadowColorFromDataset: false // optional
       },
 
-      data : [
+      Olliedata : [
         {
-          Trick: "Ollie lands",
+          name: "Ollie lands",
           number: 15,
           color: "rgba(131, 167, 234, 1)",
           legendFontColor: "#7F7F7F",
@@ -72,8 +74,41 @@ class Sensors extends Component {
           legendFontColor: "#7F7F7F",
           legendFontSize: 15
         }
-      ]
-    
+      ],
+      Kickflipdata : [
+        {
+          name: "Kickflip lands",
+          number: 13,
+          color: "rgba(131, 167, 234, 1)",
+          legendFontColor: "#7F7F7F",
+          legendFontSize: 15
+        },
+        {
+          name: "Kickflip attempts",
+          number: 40,
+          color: "#F00",
+          legendFontColor: "#7F7F7F",
+          legendFontSize: 15
+        }
+      ],
+      
+      Heelflipdata : [
+        {
+          name: "Heelflip lands",
+          number: 5,
+          color: "rgba(131, 167, 234, 1)",
+          legendFontColor: "#7F7F7F",
+          legendFontSize: 15
+        },
+        {
+          name: "Heelflip attempts",
+          number: 32,
+          color: "#F00",
+          legendFontColor: "#7F7F7F",
+          legendFontSize: 15
+        }
+      ],
+      data:[]
     }
 
     
@@ -82,78 +117,70 @@ class Sensors extends Component {
       return new Promise(resolve => setTimeout(resolve, ms));
     }
  
+    onTricksRecieved = (tricks)=>{
+     
+        this.setState(prevState => ({
+            tricks: prevState.data = tricks
+            
+        }));
+
+       // console.log(messages)
+
+    
+    }
+
+    setData= (data)=>{
+     
+      this.setState(prevState => ({
+          data: prevState.data = data
+          
+      }));
+
+     // console.log(messages)
+
   
-//   Fast =() => {
+  }
+  
 
+    filter = (itemValue, itemIndex) =>{
+      this.setState({filter: itemValue});
+      // store.dispatch(filter(itemValue))
+  
+  if(this.state.filter==="Ollie"){
+    this.setData(this.state.Olliedata);
+     // getTricks(this.onTricksRecieved,"Ollie");
+  }
+  else if(this.state.filter==="Kickflip") {
+      //getTricks(this.onTricksRecieved, "Kickflip");
+      this.setData(this.state.Kickflipdata);
 
-   
-//       // for trick detection wait for a spike in the y axis speed and
-//   // if the x axis doesn't go below threshold count the trick as landed otherwise it is an attempt
+  }
+  else if(this.state.filter==="Heelflip") {
+   // getTricks(this.onTricksRecieved, "Heelflip");
+   this.setData(this.state.Heelflipdata);
 
-// //jump flight time formula =  Math.sqrt(Math.pow(accelerometerData.x,2)+ Math.pow(accelerometerData.y,2) + Math.pow(accelerometerData.z,2)) 
-// //wait for timeout period to calculate
-// Accelerometer.setUpdateInterval(5000);
+}
+}
 
-// //   console.log(accelerometerData.y);
-// //   console.log(this.state.Barometer);
-// var ySpeed = this.state.accelerometerData.y;
-// var xSpeed = this.state.accelerometerData.x;
-// var zSpeed = this.state.accelerometerData.z;
+    MyPieChart = () => {
+      return (
+        <>
+          <Text style={styles.header}>Pie Chart</Text>
+          <PieChart
+            data={this.state.data}
+            width={Dimensions.get('window').width - 16}
+            height={220}
+            chartConfig={this.state.chartConfig}
+            accessor="number"
+            backgroundColor="transparent"
+            paddingLeft="15"
+            absolute //for the absolute number remove if you want percentage
+          />
+        </>
+      );
+    };
+
     
-    
-//       // DeviceMotion.setUpdateInterval(100);
-//       //for rotation if the starting rotation increases 360 or 180 +1 for 360 and +.5 for 180
-//       // var gyro = this.state.DeviceData.rotation
-//       // console.log(gyro);
-//     //  var  yaw = Math.atan2(this.state.DeviceData.rotation.y, -(this.state.DeviceData.rotation.z)) * 180 / Math.PI;// In degrees
-   
-// // console.log('degrees: '+yaw)
-//     let flightTime = Math.sqrt(Math.pow(xSpeed,2)+ Math.pow(ySpeed,2) + Math.pow(zSpeed,2)) 
-
-//     // var alt =this.state.Barometer.relativeAltitude;
-//     if(ySpeed>0.5){
-//     console.log('------------------------');
-//     // console.log('Yaw: ' + yaw);
-//     console.log('y:' + ySpeed.toFixed(3));
-//     console.log('x:' + xSpeed.toFixed(3));
-//     console.log('z:' + zSpeed.toFixed(3));
-
-
-//     // console.log(alt.toFixed(3));
-//     console.log('------------------------');
-//     }
-//     this.setState(prevState => ({
-//         flight: prevState.flightTime = flightTime
-//       }));
-
-
-//     if(ySpeed>0.5){
-//         this.setState(prevState => ({
-//             trickAttempt: prevState.trickAttempt+1
-            
-//           }));
-//           this.setState({ DeviceData: [] });
-    
-//       }
-//       if (ySpeed>0.5 && this.state.accelerometerData.x<0.5){
-//         // this.setState({ landed: false });
-//         this.setState({ streak: 0 });
-
-//       }
-//       // Maybe only count as landed if x speed with is anything greater .5  of what it was before the y axis increase in speed or if is a max of .5 slower
-//      if (ySpeed>0.5 && this.state.accelerometerData.x>0.5){
-//         this.setState(prevState => ({
-//             trickCount: prevState.trickCount+1,
-//             streak: prevState.streak+1
-            
-//           }));
-//           this.setState({ DeviceData: [] });
-//           // this.setState({ landed: true });
-        
-//       }
-    
-    
-//   };
 
 detectTrick= async () =>{
 DeviceMotion.setUpdateInterval(5000)
@@ -166,15 +193,15 @@ var  yaw = Math.atan2(this.state.DeviceData.rotation.gamma, -this.state.DeviceDa
 var  pitch = Math.atan2(-this.state.DeviceData.rotation.beta, -this.state.DeviceData.rotation.alpha) * 180 / Math.PI;// In degrees
 var roll = Math.atan2(-this.state.DeviceData.rotation.gamma, -this.state.DeviceData.rotation.beta) * 180 / Math.PI;// In degrees
 
-console.log('yaw : '+yaw +" degrees");
-console.log('pitch : '+pitch +" degrees");
-console.log('roll : '+roll +" degrees");
+// console.log('yaw : '+yaw +" degrees");
+// console.log('pitch : '+pitch +" degrees");
+// console.log('roll : '+roll +" degrees");
 
 
 let flightTime = Math.sqrt(Math.pow(xSpeed,2)+ Math.pow(ySpeed,2) + Math.pow(zSpeed,2)) 
-
+console.log(xSpeed)
 // var alt =this.state.Barometer.relativeAltitude;
-if(ySpeed>0.5){
+if(ySpeed>3 || ySpeed<-3){
 console.log('------------------------');
  console.log('Yaw: ' + yaw + "degrees");
 console.log('y:' + ySpeed.toFixed(3));
@@ -191,24 +218,25 @@ this.setState(prevState => ({
 
 
 
-  if (ySpeed>0.5 && xSpeed>0.5){
-    await this.sleep(2000); 
-    speed = this.state.DeviceData.acceleration.x;
-    if(xSpeed<speed){
+  if (ySpeed>10 && xSpeed>5){
+    // await this.sleep(2000); 
+    // speed = this.state.DeviceData.acceleration.x;
+    // if(xSpeed<speed){
     this.setState(prevState => ({
       trickAttempt: prevState.trickAttempt+1,
         trickCount: prevState.trickCount+1,
         streak: prevState.streak+1
       }));
-    }}
-    else if(ySpeed>0.5 && xSpeed<0.5){
+      await this.sleep(3000);
+    }//}
+    else if(ySpeed>10 && xSpeed<0.4){
       this.setState(prevState => ({
           trickAttempt: prevState.trickAttempt+1
         }));
         await this.sleep(3000);
   
     }
-  if (ySpeed>0.5 && xSpeed<0.5){
+  if (ySpeed>10 && xSpeed<0.4){
     // this.setState({ landed: false });
     this.setState({ streak: 0 });
     
@@ -225,8 +253,13 @@ this.setState(prevState => ({
 
 
         
-
-
+    
+ontTricksRecieved = (data)=>{
+  // console.log(coordinates);
+    this.setState(prevState => ({
+        data: prevState.data = data
+    }));
+  }
 
 
 DeviceMotionSub = () => {
@@ -246,41 +279,36 @@ _subscribe = () => {
     this.Fast();
   });
 }
-_unsubscribe = () => {
-  this.DeviceMotionSub && this.DeviceMotionSub.remove();
-  this.DeviceMotionSub = null;
-}
 
-componentWillUnmount() {
- 
-  this._unsubscribe();
-}
+
+
 
 componentDidMount() {
   this.DeviceMotionSub();
-}
-
-componentWillMount() {
-  // const { width, height } = Dimensions.get('window');
-  // this.screenWidth = width;
-  // this.screenHeight = height;
-  // this.boxWidth = this.screenWidth / 10.0;
-}
-
-  filter = (itemValue, itemIndex) =>{
-    this.setState({filter: itemValue});
-    // store.dispatch(filter(itemValue))
-
-
+  //getTricks(this.onTricksRecieved);
 
 }
+
+
+
+// renderCarouselItem = ({item}) => 
+      
+// <View style= {styles.cardContainer}
+// >
+
+//     <Text style={styles.cardTitle}>{item.name}
+//     </Text>
+//     {/* <Image style={styles.cardImage} source={{uri: item.image}}
+//    /> */}
+// </View>
 
 
 
   render() {
 
     return (
-      <ScrollView style={styles.containerStyle}>
+      <View style={styles.containerStyle}>
+      <ScrollView style={styles.containerStyle} >
 
       {/* <View style={styles.textContainerStyle}>
       <Text>Attempts: {this.state.trickAttempt}</Text>
@@ -294,12 +322,16 @@ componentWillMount() {
       </View> */}
       {/* <Button onPress={this.Fast()}>
           <Text>Start</Text></Button> */}
+
+
+          
       <Grid>
       <Row style={{ height: 40,  justifyContent: 'center' }}>
         
       </Row>
 
 <Row style={{ height: 240,  justifyContent: 'center' }}>
+  
 <Picker
                  selectedValue={this.state.filter}
                      style={{ height: 50, width: 150 }}
@@ -308,15 +340,17 @@ componentWillMount() {
 
                          
                          >
-  <Picker.Item label="Select Tricks" value=" default" />
-  <Picker.Item label="Ollie" value="ollie" />
-  <Picker.Item label="Kickflip" value="kf" />
-  <Picker.Item label="FS Ollie 180" value="o180" /> 
+ 
+  <Picker.Item label="Ollie" value="Ollie" />
+  <Picker.Item label="Kickflip" value="Kickflip" />
+  <Picker.Item label="Heelflip" value="Heelflip" /> 
 
  </Picker>
 </Row>
 
 <Row style={{height:40,justifyContent: 'center'}}>
+ 
+
 <Text>Total</Text>
     </Row>
       <Row style={{ height: 40,justifyContent: 'center' }}>
@@ -329,14 +363,14 @@ componentWillMount() {
 
     <Row style={{ height: 40,justifyContent: 'center' }}>
         <Col style={{alignItems: 'center'}}>
-      <Text>Falls: {this.state.trickAttempt}</Text>
+     
       </Col>
       <Col style={{alignItems: 'center'}}>
 <Text> Streak : {this.state.streak}</Text></Col>
     </Row>
 
 
-    <Row style={{height:40,justifyContent: 'center'}}>
+    {/* <Row style={{height:40,justifyContent: 'center'}}>
 <Text>Goals</Text>
     </Row>
     <Row style={{ height: 40 ,justifyContent: 'center'}}>
@@ -345,40 +379,22 @@ componentWillMount() {
       </Col>
       <Col style={{alignItems: 'center'}}>
 <Text> Weekly : 0/50</Text></Col>
-    </Row>
+    </Row> */}
    
 
     <Row style={{height:40,justifyContent: 'center'}}>
 <Text>Graph Stats</Text>
     </Row>
-<Row style={{height:400,justifyContent: 'center'}} >
-
-      
-</Row>
-
       </Grid>
-      <PieChart
-  data={this.state.data}
-  width={screenWidth}
-  height={220}
-  chartConfig={this.state.chartConfig}
-  accessor={"number"}
-  backgroundColor={"transparent"}
-  paddingLeft={"15"}
-  center={[10, 50]}
-  absolute
-/>
+      <View style={styles.containerStyle}>
+ 
+ 
+<this.MyPieChart></this.MyPieChart>
+  </View>
   </ScrollView>
-    //   <View style={styles.textContainer}>
-    //     <Button onPress={this.Fast} >
-    //       <Text>Update</Text>
-    //     </Button>
-    //     <Text style={styles.emptyTitle}>Jump Flight Time  = {this.state.flightTime} </Text>
-    // <Text style={styles.emptyTitle}>Tricks attempted = {this.state.trickAttempt} </Text>
-    //     <Text style={styles.emptyTitle}>Tricks landed = {this.state.trickCount}</Text>
-    //     <Text style={styles.emptySubtitle}></Text>
-       
-    //   </View>
+
+  </View>
+  
      ); }
 }
 
@@ -388,10 +404,10 @@ componentWillMount() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex:1,
     alignItems: 'center',
     justifyContent: 'center',
-   
+   height:1200,
     backgroundColor: '#ecf0f1',
   },
     header:{
